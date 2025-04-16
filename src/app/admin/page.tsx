51885619a33f344bf7a798db66f52e3e6963946e
lijin lekshman <lijinlekshman1@gmail.com>
2025-04-16 06:00:00 +0000

    fix issues

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -270,7 +270,7 @@ export default function Admin() {
 
 
   return (
-    <div className="flex flex-col min-h-screen bg-gray-100">
+    
       <header className="bg-white shadow">
         <div className="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8 flex justify-between items-center">
           <h1 className="text-3xl font-bold text-gray-900">
@@ -701,7 +701,6 @@ export default function Admin() {
           </AlertDialogFooter>
         </AlertDialogContent>
       </main>
-    </div>
+    
   );
 }
-
